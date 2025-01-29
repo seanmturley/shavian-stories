92d0d23a388ee0252c3fb9ab6b71c3b0b1c43ab8
Sean Turley <seanmturley@gmail.com>
2025-01-29 21:37:23 +1300

    [#9] Add author metadata mockup

---
+++ src/library/h-p-lovecraft/metadata.ts
@@ -0,0 +1,20 @@
+export const author: Author = {
+  latin: "H. P. Lovecraft",
+  shavian: "𐑣. ·𐑐. ·𐑤𐑳𐑝𐑒𐑮𐑭𐑓𐑑",
+  url: "/read/h-p-lovecraft"
+};
+
+export const stories: Stories = {
+  dagon: {
+    title: { latin: "Dagon", shavian: "·𐑛𐑨𐑜𐑩𐑯", url: "dagon" },
+    year: 1919
+  },
+  theDunwichHorror: {
+    title: {
+      latin: "The Dunwich Horror",
+      shavian: "𐑞 ·𐑛𐑳𐑯𐑢𐑦𐑗 𐑣𐑪𐑮𐑼",
+      url: "the-dunwich-horror"
+    },
+    year: 1929
+  }
+};
