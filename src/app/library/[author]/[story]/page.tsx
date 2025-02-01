45d104ba13e2217432cfd615f78f6f8e243499fe
Sean Turley <seanmturley@gmail.com>
2025-02-01 13:03:38 +1300

    [#11] Add generateStaticParams to StoryPage

--- src/app/library/[author]/[story]/page.tsx
+++ src/app/library/[author]/[story]/page.tsx
@@ -1,4 +1,19 @@
 import Story from "@components/story";
+import metadata from "@library/metadata";
+
+export function generateStaticParams() {
+  const staticParams = [];
+
+  for (const author in metadata) {
+    const stories = metadata[author].stories;
+
+    for (const story in stories) {
+      staticParams.push({ author, story });
+    }
+  }
+
+  return staticParams;
+}
 
 export default async function StoryPage({
   params
