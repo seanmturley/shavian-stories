7054ed8f3ec834ad66f2739f2cf08a073c1cb15a
Sean Turley <seanmturley@gmail.com>
2025-02-02 17:30:07 +1300

    [#16] Add sanitizeLatinText utility function

--- src/utils/create-tooltip-markup.ts
+++ src/utils/create-tooltip-markup.ts
@@ -1,3 +1,19 @@
+export function sanitizeLatinText(latin: string) {
+  const startOfLineSpaces = /^\s+/gm;
+  const consecutiveSpaces = /[^\S\r\n][^\S\r\n]+/g;
+  const latinTrimmed = latin
+    .replace(consecutiveSpaces, " ")
+    .replace(startOfLineSpaces, "\n")
+    .trim();
+
+  const speechMarks = /(?<=^|\p{L}\p{P}\s)['"‘“](.+?)(?<=\p{L}\p{P}+)['"’”]/gmu;
+  const quotationMarks = /(?<!\p{L})(['"])([^'"]*)\1(?!\p{L})/gu;
+
+  return latinTrimmed
+    .replaceAll(speechMarks, "«$1»")
+    .replaceAll(quotationMarks, "‹$2›");
+}
+
 export function createStoryMarkup(latin: string, shavian: string) {
   const shavianTidy = replaceQuotesAndApostrophes(shavian);
 
