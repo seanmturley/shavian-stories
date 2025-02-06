64df7a5f6a2ba51e5829a6c0d53aa3f013bffbb7
Sean Turley <seanmturley@gmail.com>
2025-02-06 22:24:59 +1300

    [#16] Add form to /format-text that generates story HTML

--- src/app/format-text/page.tsx
+++ src/app/format-text/page.tsx
@@ -2,11 +2,17 @@
 
 import { useActionState, useEffect } from "react";
 import SubmitButton from "@components/submit-button";
-import { formatLatin } from "@utils/format-text-form-actions";
+import { formatLatin, getStoryHtml } from "@utils/format-text-form-actions";
 
 const initialFormatLatinState = {
   latin: "",
   latinSanitized: "",
+  message: ""
+};
+
+const initialStoryHtmlState = {
+  shavian: "",
+  storyHtml: "",
   message: ""
 };
 
@@ -16,37 +22,88 @@ export default function FormatText() {
     initialFormatLatinState
   );
 
+  const [storyHtmlState, storyHtmlAction] = useActionState(
+    getStoryHtml,
+    initialStoryHtmlState
+  );
+
   useEffect(() => {
+    if (storyHtmlState.storyHtml) {
+      navigator.clipboard.writeText(storyHtmlState.storyHtml);
+      return;
+    }
+
     if (formatLatinState.latinSanitized) {
       navigator.clipboard.writeText(formatLatinState.latinSanitized);
     }
   });
 
   return (
-    <form>
-      <label htmlFor="latin">Enter marked up Latin:</label>
-      <textarea
-        autoComplete="off"
-        autoCorrect="off"
-        // autoFocus
-        cols={100}
-        defaultValue={formatLatinState.latin || ""}
-        name="latin"
-        required
-        rows={30}
-        spellCheck={false}
-      />
+    <>
+      <section>
+        <form>
+          <label htmlFor="latin">Enter marked up Latin:</label>
+          <textarea
+            autoComplete="off"
+            autoCorrect="off"
+            autoFocus
+            cols={100}
+            defaultValue={formatLatinState.latin || ""}
+            id="latin"
+            name="latin"
+            required
+            rows={30}
+            spellCheck={false}
+          />
 
-      <SubmitButton
-        formAction={formatLatinAction}
-        pendingText="Formatting text..."
-      >
-        Get formatted Latin
-      </SubmitButton>
+          <SubmitButton
+            formAction={formatLatinAction}
+            pendingText="Formatting text..."
+          >
+            Get formatted Latin
+          </SubmitButton>
 
-      <div aria-live="polite">
-        {formatLatinState.message && <p>{formatLatinState.message}</p>}
-      </div>
-    </form>
+          <div aria-live="polite">
+            {formatLatinState.message && <p>{formatLatinState.message}</p>}
+          </div>
+        </form>
+      </section>
+
+      <section>
+        <form>
+          <label htmlFor="shavian">Enter marked up Shavian</label>
+          <textarea
+            autoComplete="off"
+            autoCorrect="off"
+            cols={100}
+            defaultValue={storyHtmlState.shavian || ""}
+            disabled={!formatLatinState.latinSanitized}
+            id="shavian"
+            name="shavian"
+            required
+            rows={30}
+            spellCheck={false}
+          />
+
+          <input
+            type="hidden"
+            name="latinSanitized"
+            value={formatLatinState.latinSanitized}
+          />
+
+          <SubmitButton
+            disabled={!formatLatinState.latinSanitized}
+            formAction={storyHtmlAction}
+            pendingText="Formatting text..."
+          >
+            Get story HTML
+          </SubmitButton>
+
+          <div aria-live="polite">
+            {storyHtmlState.message && <p>{storyHtmlState.message}</p>}
+          </div>
+        </form>
+      </section>
+    </>
   );
 }
--- src/utils/format-text-form-actions.ts
+++ src/utils/format-text-form-actions.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { sanitizeLatinText } from "@utils/format-text";
+import { createStoryMarkup, sanitizeLatinText } from "@utils/format-text";
 
 export const formatLatin: FormAction = async function (prevState, formData) {
   const latin = formData.get("latin") as string;
@@ -13,4 +13,28 @@ export const formatLatin: FormAction = async function (prevState, formData) {
     latinSanitized,
     message: "Formatted Latin text copied to clipboard."
   };
+};
+
+export const getStoryHtml: FormAction = async function (prevState, formData) {
+  const shavian = formData.get("shavian") as string;
+  const latinSanitized = formData.get("latinSanitized") as string;
+
+  try {
+    const storyHtml = createStoryMarkup(latinSanitized, shavian);
+
+    return {
+      ...prevState,
+      shavian,
+      storyHtml,
+      message: "Story HTML copied to clipboard."
+    };
+  } catch (error) {
+    if (error instanceof Error) {
+      return {
+        ...prevState,
+        shavian,
+        message: `Error: ${error.message}.`
+      };
+    }
+  }
 };
