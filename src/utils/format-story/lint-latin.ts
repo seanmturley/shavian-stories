0d3fc637b73b153c1f3d73b3354e587255492042
Sean Turley <seanmturley@gmail.com>
2025-02-07 17:27:49 +1300

    [#22] Add JSDoc documentation to lintLatin

--- src/utils/format-story/lint-latin.ts
+++ src/utils/format-story/lint-latin.ts
@@ -1,3 +1,24 @@
+/**
+ * Lints the provided Latin text, including replacing conventional speech marks
+ * and quotes with guillemets.
+ *
+ * @remarks
+ * The linting process involves the following steps:
+ *  - Removing all extra whitespace
+ *  - Replacing all speech marks (single or double, including fancy variants)
+ *    with double guillemets.
+ *  - Replacing all quotation marks (single or double, including fancy variants)
+ *    with single guillemets.
+ *
+ * **NOTE:** All apostrophes will be left intact *except* in the rare situation
+ * where there is a leading apostrophe followed by a trailing apostrophe e.g.:
+ * "'tis the boys' favourite."
+ *
+ * @param latin - A string of Latin text, copied from the raw story source text,
+ * with additional markup added as necessary. See {@link generateStoryHtml} for
+ * markup details.
+ * @returns A linted version of the provided Latin text.
+ */
 export default function lintLatin(latin: string) {
   const startOfLineSpaces = /^\s+/gm;
   const consecutiveSpaces = /[^\S\r\n][^\S\r\n]+/g;
