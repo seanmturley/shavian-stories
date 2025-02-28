75fffaafdd10b50a03412720b5d64e0ce1de6e1a
Sean Turley <seanmturley@gmail.com>
2025-02-28 21:41:40 +1300

    [#54] Add table filtering

--- src/components/table.tsx
+++ src/components/table.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import {
   flexRender,
   getCoreRowModel,
+  getFilteredRowModel,
   getSortedRowModel,
   useReactTable,
   type ColumnDef,
@@ -22,6 +23,7 @@ export default function Table({
   data,
   defaultSortColumn
 }: TableProps) {
+  const [globalFilter, setGlobalFilter] = useState("");
   const [sorting, setSorting] = useState<SortingState>([
     { id: defaultSortColumn, desc: false }
   ]);
@@ -33,44 +35,54 @@ export default function Table({
     getSortedRowModel: getSortedRowModel(),
     onSortingChange: setSorting,
     enableSortingRemoval: false,
-    state: { sorting }
+    getFilteredRowModel: getFilteredRowModel(),
+    onGlobalFilterChange: setGlobalFilter,
+    globalFilterFn: "includesString",
+    state: { globalFilter, sorting }
   });
 
   return (
-    <table>
-      <thead>
-        {table.getHeaderGroups().map((headerGroup) => (
-          <tr key={headerGroup.id}>
-            {headerGroup.headers.map((header) => (
-              <th key={header.id}>
-                <button
-                  onClick={header.column.getToggleSortingHandler()}
-                  title={getSortTitle(header.column)}
-                >
-                  <span>
-                    {flexRender(
-                      header.column.columnDef.header,
-                      header.getContext()
-                    )}
-                  </span>
-                  <span>{getSortIcon(header.column)}</span>
-                </button>
-              </th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody>
-        {table.getRowModel().rows.map((row) => (
-          <tr key={row.id}>
-            {row.getVisibleCells().map((cell) => (
-              <td key={cell.id}>
-                {flexRender(cell.column.columnDef.cell, cell.getContext())}
-              </td>
-            ))}
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <div>
+      <input
+        onChange={(e) => setGlobalFilter(e.target.value)}
+        placeholder="Search..."
+        value={globalFilter}
+      />
+      <table>
+        <thead>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <th key={header.id}>
+                  <button
+                    onClick={header.column.getToggleSortingHandler()}
+                    title={getSortTitle(header.column)}
+                  >
+                    <span>
+                      {flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                    </span>
+                    <span>{getSortIcon(header.column)}</span>
+                  </button>
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody>
+          {table.getRowModel().rows.map((row) => (
+            <tr key={row.id}>
+              {row.getVisibleCells().map((cell) => (
+                <td key={cell.id}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
