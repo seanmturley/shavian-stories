836c3736d431cc95583259814000e4f76da34f51
Sean Turley <seanmturley@gmail.com>
2025-05-11 22:44:45 +1200

    [#70] Wrap NavBar in a 'header' and separate 'h1' and 'nav' elements

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -3,40 +3,44 @@ import styles from "./nav-bar.module.css";
 
 export default function NavBar() {
   return (
-    <nav>
-      <Link href="/">
-        <h1>Shavian Stories</h1>
-      </Link>
-      <ul>
-        <li className={styles.dropdown}>
-          <Link href="#">Learn</Link>
-          <ul className={styles.dropdownContent}>
-            <li>
-              <Link href="#">Shavian</Link>
-            </li>
-            <li>
-              <Link href="#">Neo-Shavian English</Link>
-            </li>
-            <li>
-              <Link href="#">Neo-Shavian French</Link>
-            </li>
-          </ul>
-        </li>
-        <li className={styles.dropdown}>
-          <Link href="/library">Read</Link>
-          <ul className={styles.dropdownContent}>
-            <li>
-              <Link href="/library">Shavian</Link>
-            </li>
-            <li>
-              <Link href="/library">Neo-Shavian English</Link>
-            </li>
-            <li>
-              <Link href="/library">Neo-Shavian French</Link>
-            </li>
-          </ul>
-        </li>
-      </ul>
-    </nav>
+    <header>
+      <h1>
+        <Link href="/">Shavian Stories</Link>
+      </h1>
+
+      <nav>
+        <ul>
+          <li className={styles.dropdown}>
+            <Link href="#">Learn</Link>
+            <ul className={styles.dropdownContent}>
+              <li>
+                <Link href="#">Shavian</Link>
+              </li>
+              <li>
+                <Link href="#">Neo-Shavian English</Link>
+              </li>
+              <li>
+                <Link href="#">Neo-Shavian French</Link>
+              </li>
+            </ul>
+          </li>
+
+          <li className={styles.dropdown}>
+            <Link href="/library">Read</Link>
+            <ul className={styles.dropdownContent}>
+              <li>
+                <Link href="/library">Shavian</Link>
+              </li>
+              <li>
+                <Link href="/library">Neo-Shavian English</Link>
+              </li>
+              <li>
+                <Link href="/library">Neo-Shavian French</Link>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
